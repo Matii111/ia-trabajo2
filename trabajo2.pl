1120e0b45639af57abac75cffeb41403b495f707
matii111 <m.chaparrodaz@uandresbello.edu>
2022-10-05 19:49:57 -0300

    avance-05-oct

---
+++ trabajo2.pl
@@ -0,0 +1,49 @@
+
+%Hechos=========================
+ballena(azul)         		.
+ballena(fin)          	    .
+ballena(jorobada)     	    .
+
+delfin(austral)        		.
+delfin(comun)          		.
+delfin(cruzado)        		.
+lobo(finoAustral)      		.
+lobo(marinoComun)      		.	
+
+cachalote(cachalote)   		.
+orca(orca)             		.
+
+conservacion(a)        		.
+conservacion(v)        		.
+conservacion(ca)       		.
+conservacion(pm)       		.
+conservacion(di)       		.
+conservacion(ne)       		.
+
+distribucion(pas)      		.
+distribucion(jfe)      	 	.
+distribucion(des)      	 	.
+distribucion(dr)		    .
+
+profundidad(sublitoral)     .
+profundidad(taludSuperior)  .
+profundidad(taludInferior)  .
+profundidad(neritico)		.
+profundidad(pelagico)		.
+profundidad(mesoPelagico)   .
+profundidad(oceanico)		.
+
+extraccion(arrastre)		.
+extraccion(buceo)			.
+extraccion(cerco)			.
+extraccion(enmalle)			.
+extraccion(espinel)			.
+extraccion(lineaMano)		.
+extraccion(potera)			.
+extraccion(trampas)			.
+
+%Reglas=========================
+
+proc:-
+	write("Ingrese el estado de conservacion: ")proc
+
